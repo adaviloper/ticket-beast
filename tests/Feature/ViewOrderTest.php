238d515c5c9f5962982cd9ed6b2308150ec2d06d
adrian <adavila.dev@gmail.com>
2020-11-28 14:32:31 -0600

    12.2 - Driving out the Endpoint

--- tests/Feature/ViewOrderTest.php
+++ tests/Feature/ViewOrderTest.php
@@ -14,13 +14,16 @@ class ViewOrderTest extends \Tests\TestCase
     /** @test */
     public function user_can_view_their_order_confirmation()
     {
+        $this->disableExceptionHandling();
         $concert = factory(Concert::class)->create();
-        $order = factory(Order::class)->create();
+        $order = factory(Order::class)->create(['confirmation_number' => 'ORDER-CONFIRMATION-1234']);
         $ticket = factory(Ticket::class)->create([
             'concert_id' => $concert->id,
             'order_id' => $order->id,
         ]);
 
-        $this->get("orders/{$order->id}");
+        $response = $this->get("orders/{$order->confirmation_number}");
+
+        $response->assertStatus(200);
     }
 }
