73c28665378e75b78b7fbf43c1b3f95b88a560eb
adrian <16654653+adaviloper@users.noreply.github.com>
2020-11-28 21:58:56 -0600

    13.5 - Testing the Confirmation Number Format

---
+++ tests/Unit/RandomOrderConfirmationNumberGeneratorTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\RandomOrderConfirmationNumberGenerator;
+use Tests\TestCase;
+
+class RandomOrderConfirmationNumberGeneratorTest extends TestCase
+{
+    /** @test */
+    public function can_only_contain_uppercase_letters_and_numbers(): void
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator();
+
+        $confirmationNumber = $generator->generate();
+
+        self::assertRegExp('/^[A-Z0-9]/', $confirmationNumber);
+    }
+
+    /** @test */
+    public function cannot_contain_ambiguous_characters(): void
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator();
+
+        $confirmationNumber = $generator->generate();
+
+        self::assertRegExp('/^[A-Z0-9]/', $confirmationNumber);
+        self::assertFalse(strpos($confirmationNumber, '1'));
+        self::assertFalse(strpos($confirmationNumber, 'I'));
+        self::assertFalse(strpos($confirmationNumber, '0'));
+        self::assertFalse(strpos($confirmationNumber, 'O'));
+    }
+
+    /** @test */
+    public function must_be_24_characters_long(): void
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator();
+
+        $confirmationNumber = $generator->generate();
+
+        self::assertEquals(24, strlen($confirmationNumber));
+    }
+
+    /** @test */
+    public function all_order_confirmation_numbers_must_be_unique(): void
+    {
+
+    }
+}
