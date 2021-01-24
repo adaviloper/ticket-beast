c49263698eb2bc8743d28ebc66b0822ae5d7973f
adrian <16654653+adaviloper@users.noreply.github.com>
2021-01-23 22:03:25 -0600

    corrected file name

--- app/Http/Controllers/ConcertsController.php
+++ app/Http/Controllers/ConcertsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Concert;
 use Illuminate\Http\Request;
 
-class ConcertController extends Controller
+class ConcertsController extends Controller
 {
     public function show($id)
     {
