0afae89503a4779dcaac5cf37632eb7eba1a9512
adrian <16654653+adaviloper@users.noreply.github.com>
2021-01-23 23:20:35 -0600

    18.4 - Converting Empty Strings to Null

--- tests/Feature/Backstage/AddConcertTest.php
+++ tests/Feature/Backstage/AddConcertTest.php
@@ -133,4 +133,45 @@ class AddConcertTest extends TestCase
         $response->assertSessionHasErrors('title');
         self::assertEquals(0, Concert::count());
     }
+
+    /** @test */
+    public function subtitle_is_optional(): void
+    {
+        $this->disableExceptionHandling();
+
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->post('/backstage/concerts', [
+                'title' => 'No Warning',
+                'subtitle' => '',
+                'additional_information' => "You must be 19 years of age to attend this concert.",
+                'date' => '2017-11-18',
+                'time' => '8:00pm',
+                'venue' => 'The Mosh Pit',
+                'venue_address' => '123 Fake St.',
+                'city' => 'Laraville',
+                'state' => 'ON',
+                'zip' => '12345',
+                'ticket_price' => '32.50',
+                'ticket_quantity' => '75',
+            ]);
+
+        tap(Concert::first(), function ($concert) use ($response) {
+            $response->assertStatus(302);
+            $response->assertRedirect("/concerts/{$concert->id}");
+
+            self::assertEquals('No Warning', $concert->title);
+            self::assertNull($concert->subtitle);
+            self::assertEquals("You must be 19 years of age to attend this concert.", $concert->additional_information);
+            self::assertEquals(Carbon::parse('2017-11-18 8:00pm'), $concert->date);
+            self::assertEquals('The Mosh Pit', $concert->venue);
+            self::assertEquals('123 Fake St.', $concert->venue_address);
+            self::assertEquals('Laraville', $concert->city);
+            self::assertEquals('ON', $concert->state);
+            self::assertEquals('12345', $concert->zip);
+            self::assertEquals(3250, $concert->ticket_price);
+            self::assertEquals(75, $concert->ticketsRemaining());
+        });
+    }
 }
