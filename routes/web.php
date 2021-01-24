4844213bfa69de85891d6b13867e05e707cd6bf9
adrian <16654653+adaviloper@users.noreply.github.com>
2021-01-23 22:45:36 -0600

    18.2 - Adding a Valid Concert

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('', static function () {
     return 'Laravel';
 });
 
-Route::get('concerts/{id}', 'ConcertsController@show');
+Route::get('concerts/{id}', 'ConcertsController@show')->name('concerts.show');
 Route::post('concerts/{id}/orders', 'ConcertOrdersController@store');
 Route::get('orders/{confirmationNumber}', 'OrdersController@show');
 
@@ -23,6 +23,7 @@ Route::get('/login', 'Auth\LoginController@showLoginForm')->name('auth.show-logi
 Route::post('/login', 'Auth\LoginController@login')->name('auth.login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('auth.logout');
 
-Route::group(['middleware' => ['auth']], static function () {
-    Route::get('/backstage/concerts/new', 'Backstage\ConcertsController@create');
+Route::group(['middleware' => ['auth'], 'namespace' => 'Backstage', 'prefix' => 'backstage'], static function () {
+    Route::get('concerts/new', 'ConcertsController@create');
+    Route::post('concerts', 'ConcertsController@store');
 });
--- tests/Feature/Backstage/AddConcertTest.php
+++ tests/Feature/Backstage/AddConcertTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature\Backstage;
 
+use App\Concert;
 use App\User;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -29,5 +31,69 @@ class AddConcertTest extends TestCase
 
         $response->assertStatus(302);
         $response->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function adding_a_valid_concert(): void
+    {
+        $this->disableExceptionHandling();
+
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->post('/backstage/concerts', [
+                'title' => 'No Warning',
+                'subtitle' => 'with Cruel Hand and Backtrack',
+                'additional_information' => "You must be 19 years of age to attend this concert.",
+                'date' => '2017-11-18',
+                'time' => '8:00pm',
+                'venue' => 'The Mosh Pit',
+                'venue_address' => '123 Fake St.',
+                'city' => 'Laraville',
+                'state' => 'ON',
+                'zip' => '12345',
+                'ticket_price' => '32.50',
+                'ticket_quantity' => '75',
+            ]);
+
+        tap(Concert::first(), function ($concert) use ($response) {
+            $response->assertStatus(302);
+            $response->assertRedirect("/concerts/{$concert->id}");
+
+            self::assertEquals('No Warning', $concert->title);
+            self::assertEquals('with Cruel Hand and Backtrack', $concert->subtitle);
+            self::assertEquals("You must be 19 years of age to attend this concert.", $concert->additional_information);
+            self::assertEquals(Carbon::parse('2017-11-18 8:00pm'), $concert->date);
+            self::assertEquals('The Mosh Pit', $concert->venue);
+            self::assertEquals('123 Fake St.', $concert->venue_address);
+            self::assertEquals('Laraville', $concert->city);
+            self::assertEquals('ON', $concert->state);
+            self::assertEquals('12345', $concert->zip);
+            self::assertEquals(3250, $concert->ticket_price);
+            self::assertEquals(75, $concert->ticketsRemaining());
+        });
+    }
+
+    /** @test */
+    public function guests_cannot_add_new_concerts(): void
+    {
+        $response = $this->post('/backstage/concerts', [
+            'title' => 'No Warning',
+            'subtitle' => 'with Cruel Hand and Backtrack',
+            'additional_information' => "You must be 19 years of age to attend this concert.",
+            'date' => '2017-11-18',
+            'time' => '8:00pm',
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Fake St.',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '12345',
+            'ticket_price' => '32.50',
+            'ticket_quantity' => '75',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+        self::assertEquals(0, Concert::count());
     }
 }
