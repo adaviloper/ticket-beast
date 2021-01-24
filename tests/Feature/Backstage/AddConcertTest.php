ed58e2d6daa9949dfe7519d98d5c9119b989f09c
adrian <16654653+adaviloper@users.noreply.github.com>
2021-01-23 22:28:36 -0600

    18.1 - Preventing Guests from Adding Concerts

--- tests/Feature/Backstage/AddConcertTest.php
+++ tests/Feature/Backstage/AddConcertTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Backstage;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -10,4 +11,23 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class AddConcertTest extends TestCase
 {
     use DatabaseMigrations;
+
+    /** @test */
+    public function promoters_can_view_the_add_concert_form(): void
+    {
+        $user = factory(User::class)->create();
+
+        $response =  $this->actingAs($user)->get('/backstage/concerts/new');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function guests_cannot_view_the_add_concert_form(): void
+    {
+        $response =  $this->get('/backstage/concerts/new');
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
 }
