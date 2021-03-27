c8f7efc7cd2d223d59050421e14837f2693327bc
adrian <16654653+adaviloper@users.noreply.github.com>
2021-03-27 16:45:42 -0500

    24.3 - Splitting Large Tests

--- tests/Feature/Backstage/ViewPublishedConcertOrdersTest.php
+++ tests/Feature/Backstage/ViewPublishedConcertOrdersTest.php
@@ -20,6 +20,19 @@ class ViewPublishedConcertOrdersTest extends TestCase
         $user = factory(User::class)->create();
         $concert = ConcertFactory::createPublished(['user_id' => $user->id]);
 
+        $response = $this->actingAs($user)->get("/backstage/published-concerts/{$concert->id}/orders");
+
+        $response->assertStatus(200);
+        $response->assertViewIs('backstage.published-concert-orders.index');
+        self::assertTrue($response->data('concert')->is($concert));
+    }
+
+    /** @test */
+    public function a_promoter_can_view_the_10_most_recent_orders_for_their_concert(): void
+    {
+        $this->disableExceptionHandling();
+        $user = factory(User::class)->create();
+        $concert = ConcertFactory::createPublished(['user_id' => $user->id]);
         $oldOrder = OrderFactory::createForConcert($concert, ['created_at' => Carbon::parse('11 days ago')]);
         $recentOrder1 = OrderFactory::createForConcert($concert, ['created_at' => Carbon::parse('10 days ago')]);
         $recentOrder2 = OrderFactory::createForConcert($concert, ['created_at' => Carbon::parse('9 days ago')]);
@@ -33,10 +46,6 @@ class ViewPublishedConcertOrdersTest extends TestCase
         $recentOrder10 = OrderFactory::createForConcert($concert, ['created_at' => Carbon::parse('1 days ago')]);
 
         $response = $this->actingAs($user)->get("/backstage/published-concerts/{$concert->id}/orders");
-
-        $response->assertStatus(200);
-        $response->assertViewIs('backstage.published-concert-orders.index');
-        self::assertTrue($response->data('concert')->is($concert));
 
         $response->data('orders')->assertNotContains($oldOrder);
         $response->data('orders')->assertEquals([
