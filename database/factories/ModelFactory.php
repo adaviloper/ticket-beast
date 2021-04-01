74a1d06074d9e1806e69772dc4f9e78bcf3d27bb
adrian <16654653+adaviloper@users.noreply.github.com>
2021-03-31 21:57:45 -0500

    30.7 - Updating Factories and a Speed Trick

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -20,8 +20,10 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 
     return [
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => '$2y$10$WxnvSPB7EH0roXcfBaUhtOMNWmXOAo44xDklD2HcFsRUieUZcDFD6', // 'secret'
         'remember_token' => str_random(10),
+        'stripe_account_id' => 'test_acct_1234',
+        'stripe_access_token' => 'test_token',
     ];
 });
 
